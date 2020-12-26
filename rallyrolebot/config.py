99d6fb9a4d6e580008980f15a952e56e62eab232
Ju99ernaut <brendonzee@gmail.com>
2020-12-26 15:52:28 +0200

    Fix conflicting options

--- rallyrolebot/config.py
+++ rallyrolebot/config.py
@@ -19,9 +19,9 @@ arg_parser.add(
     help="An SQLAlchemy connection string",
 )
 
-arg_parser.add("-h", "--host", default="127.0.0.1", help="Bind socket to this host")
+arg_parser.add("--host", default="127.0.0.1", help="Bind socket to this host")
 
-arg_parser.add("-o", "--port", default="8000", help="Bind socket to this port")
+arg_parser.add("--port", default="8000", help="Bind socket to this port")
 
 
 def parse_args():
