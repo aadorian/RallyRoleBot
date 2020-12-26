d62b569c30358063af2106b4a7d8f8c3121cfef5
Ju99ernaut <brendonzee@gmail.com>
2020-12-26 15:27:20 +0200

    Update constants.py

--- rallyrolebot/constants.py
+++ rallyrolebot/constants.py
@@ -12,6 +12,9 @@ RALLY_CONNECTIONS_TABLE = "rally_connections"
 CHANNEL_PREFIXES_TABLE = "channel_prefixes"
 DEFAULT_COIN_TABLE = "default_coin"
 CONFIG_TABLE = "config"
+USERS_TABLE = "users"
+USERS_TOKEN_TABLE = "users_token"
+COMMANDS_TABLE = "commands"
 
 
 GUILD_ID_KEY = "guildId"
@@ -21,6 +24,14 @@ ROLE_NAME_KEY = "roleName"
 CHANNEL_NAME_KEY = "channel"
 DISCORD_ID_KEY = "discordId"
 RALLY_ID_KEY = "rallyId"
+
+USERNAME_KEY = "username"
+DISCREMINATOR_KEY = "discriminator"
+GUILDS_KEY = "guilds"
+TOKEN_KEY = "token"
+TIME_CREATED_KEY = "timeCreated"
+NAME_KEY = "name"
+DESCRIPTION_KEY = "description"
 
 PREFIX_KEY = "prefix"
 
@@ -38,6 +49,7 @@ COIN_BALANCE_KEY = "coinBalance"
 
 BASE_URL = "https://api.rally.io/v1"
 COINGECKO_API_URL = "https://api.coingecko.com/api/v3"
+DISCORD_API_URL = "https://discord.com/api"
 
 
 """
@@ -62,3 +74,11 @@ PRICE_GRADIENT_DEPTH = 5
 
 DEFAULT_DONATE_MESSAGE = "You can donate to by going to - Your donation helps grow and support the community and creator - Plus, there are 10 tiers of Donation badges to earn to show off your support!"
 DEFAULT_PURCHASE_MESSAGE = "You can purchase at by using a Credit/Debit card or a number of different Crypto Currencies! Buying earns rewards, supports the community, and you can even get VIP Status! (hint: there’s a secret VIP room for users who hold over X # of ;)"
+
+API_TAGS_METADATA = [
+    {"name": "channels", "description": "Coin channel mappings"},
+    {"name": "coin", "description": "Default coin in guild"},
+    {"name": "commands", "description": "Get list of all available bot commands"},
+    {"name": "prefix", "description": "Command prefix in guild"},
+    {"name": "roles", "description": "Coin role mappings"},
+]
